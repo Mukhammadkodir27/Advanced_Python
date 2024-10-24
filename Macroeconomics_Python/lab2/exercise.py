a3933c770c5cecde8725026f10c2cfd131a595d6
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-10-24 18:40:20 +0200

    Create exercise.py

---
+++ Macroeconomics_Python/lab2/exercise.py
@@ -0,0 +1,17 @@
+# Make graphs appear within the notebook
+%matplotlib inline
+
+# Import numerical computations library
+import numpy as np
+
+# Import plotting library
+import matplotlib.pyplot as plt
+
+# Import minimization function
+from scipy.optimize import minimize
+
+# Import optimization function
+import scipy.optimize as opt
+
+# Import functions from AppliedMacroPlots.py
+from AppliedMacroPlots import *
